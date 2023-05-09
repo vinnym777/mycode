c24751ff2ad3283acce696d30ee2d15f8b2daf7d
vinnym777 <vinnym777@gmail.com>
2023-05-09 19:46:19 +0000

    Working with NASA APIs

---
+++ issloc/iss_overhead.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Alta3 Research | <your name here>
+   Using an HTTP GET to determine when the ISS will pass over head"""
+
+# python3 -m pip install requests
+import requests
+
+def main():
+    """your code goes below here"""
+    
+    # stuck? you can always write comments
+    # Try describe the steps you would take manually
+
+
+
+if __name__ == "__main__":
+    main()
+
