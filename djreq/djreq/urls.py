aa85dfb1ee9e42726d309c413042fc302c656eac
vinnym777 <vinnym777@gmail.com>
2023-05-12 17:08:46 +0000

    making requests with Django

---
+++ djreq/djreq/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# imports from Django
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('astro/', views.astro),    # access by proxy api.open-notify.org/astros.json
+    path('nasa/', views.nasa),      # access by proxy api.nasa.gov
+]
+
